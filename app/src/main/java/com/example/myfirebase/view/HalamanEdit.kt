b83f07ebf4f018000f83a38347004812ddf58e60
abimanyusatriaa <adji3890@gmail.com>
2026-01-09 20:11:58 +0700

    mengisis HalamanEdit.kt

--- app/src/main/java/com/example/myfirebase/view/HalamanEdit.kt
+++ app/src/main/java/com/example/myfirebase/view/HalamanEdit.kt
@@ -1,2 +1,15 @@
 package com.example.myfirebase.view
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.myfirebase.view.route.DestinasiEdit
+import com.example.myfirebase.viewmodel.EditViewModel
+import com.example.myfirebase.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.launch
+import androidx.compose.material3.ExperimentalMaterial3Api
+
